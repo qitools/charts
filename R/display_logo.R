163690f4c01428e9c8b09dc81a3a686c77587c76
badgettrg <rbadgett@kumc.edu>
2013-09-03 00:07:53 -0500

    Create display_logo.R

---
+++ R/display_logo.R
@@ -0,0 +1,8 @@
+display_logo <- function(x=0.8,y=-1) {
+#par(new=TRUE,xaxt="n",yaxt="n",mai=c(0,0,0,0))
+#plot(0,0,axes=F,type="n",xlab="", ylab = "",new=TRUE)
+#img <- read.bitmap(file.path('qitools','www','jayhawk.jpg'), IdentifyByExtension = TRUE)
+#mag=2
+#rasterImage(img,x, y, x + mag*45/400, y + mag*40/400, interpolate=TRUE)
+#par(xaxt="s",yaxt="s",mai=c(.956,.769,.769,.394))
+}
