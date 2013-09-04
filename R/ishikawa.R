02a307d02e466d1b89f0a545cafceb7d98ff4ee7
badgettrg <rbadgett@kumc.edu>
2013-09-03 22:50:28 -0500

    Update ishikawa.R

--- R/ishikawa.R
+++ R/ishikawa.R
@@ -1,11 +1,12 @@
 ishikawa <-
 function(content) {
-causes<-eval(parse(file = "", n = NULL, text = content))
+temp <- paste('list(',gsub('\n', '', fixed = TRUE, content),')')
+causes<-eval(parse(file = "", n = NULL, text = temp))
 KUBlue = "#0022B4"
 SkyBlue = "#6DC6E7"
 par(col.axis="black" ,col.lab=KUBlue ,col.main=KUBlue ,col.sub=KUBlue,bg=SkyBlue,xaxt="s",yaxt="s",mai=c(.956,.769,.769,.394),pin=c(14,14))
 cause.and.effect(cause=causes,title="",effect="Nonconformity",cex = c(1, 1, 0.9), font = c(2, 3, 2))
 mtext("Cause and effect diagram",        side=3,line=1,col=KUBlue,font=2, cex=1.3)
 mtext("(Ishikawa or fish-bone diagram)", side=3,line=0,col=KUBlue,font=2, cex=1)
-#display_logo(x=0.8,y=-1)
+display_logo(x=0.8,y=-1)
 }
