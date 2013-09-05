29d676b957ff0eb9fbf3d438bfd59ee3a5b78c02
badgettrg <rbadgett@kumc.edu>
2013-09-04 22:07:12 -0500

    Update ishikawa.R

--- R/ishikawa.R
+++ R/ishikawa.R
@@ -1,9 +1,11 @@
 ishikawa <-
 function(content) {
-temp <- paste('list(',gsub('\n', '', fixed = TRUE, content),')')
-causes<-eval(parse(file = "", n = NULL, text = temp))
 KUBlue = "#0022B4"
 SkyBlue = "#6DC6E7"
+temp <- paste('list(',gsub('\n', '', fixed = TRUE, content),')')
+temp <- sub("\\s+$", "", temp, perl = TRUE) #Removing trailing whitespace
+temp <- sub(",+$", "", temp, perl = TRUE) #Remove trailing comma if accidentally added by user online
+causes<-eval(parse(file = "", n = NULL, text = temp))
 par(col.axis="black" ,col.lab=KUBlue ,col.main=KUBlue ,col.sub=KUBlue,bg=SkyBlue,xaxt="s",yaxt="s",mai=c(.956,.769,.769,.394),pin=c(14,14))
 cause.and.effect(cause=causes,title="",effect="Nonconformity",cex = c(1, 1, 0.9), font = c(2, 3, 2))
 mtext("Cause and effect diagram",        side=3,line=1,col=KUBlue,font=2, cex=1.3)
