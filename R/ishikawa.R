e503e14c46c93b11b7d7e094c1dfc6b5caece1d3
badgettrg <rbadgett@kumc.edu>
2013-09-08 05:02:59 -0500

    Update ishikawa.R

--- R/ishikawa.R
+++ R/ishikawa.R
@@ -1,15 +1,14 @@
 ishikawa <-
-function(content) {
+function(content,plotbgcolor) {
 KUBlue = "#0022B4"
-SkyBlue = "#6DC6E7"
-temp <- gsub('\n', '', content, perl = TRUE)
+#SkyBlue = "#6DC6E7"
+temp <- gsub('\n', '', fixed = TRUE, content, perl = TRUE)
 temp <- gsub("\\s+$", "", temp, perl = TRUE) #Removing trailing whitespace
 temp <- gsub(",+$", "", temp, perl = TRUE) #Remove trailing comma if accidentally added by user online
 temp <- paste('list(',temp,')')
 causes<-eval(parse(file = "", n = NULL, text = temp))
-par(col.axis="black" ,col.lab=KUBlue ,col.main=KUBlue ,col.sub=KUBlue,bg=SkyBlue,xaxt="s",yaxt="s",mai=c(.956,.769,.769,.394),pin=c(14,14))
+par(col.axis="black" ,col.lab=KUBlue ,col.main=KUBlue ,col.sub=KUBlue,bg=plotbgcolor,xaxt="s",yaxt="s",mai=c(.956,.769,.769,.394),pin=c(14,14))
 cause.and.effect(cause=causes,title="",effect="Nonconformity",cex = c(1, 1, 0.9), font = c(2, 3, 2))
 mtext("Cause and effect diagram",        side=3,line=1,col=KUBlue,font=2, cex=1.3)
 mtext("(Ishikawa or fish-bone diagram)", side=3,line=0,col=KUBlue,font=2, cex=1)
-display_logo(x=0.8,y=-1)
 }
